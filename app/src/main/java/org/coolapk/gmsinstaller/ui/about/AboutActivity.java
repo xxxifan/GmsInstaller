43d7ba356bffcda9cd364cb8504ab0154e560c62
xxxifan <445947962@qq.com>
2015-04-11 23:19:43 +0800

    优化关于页面，添加FlashGordon

--- app/src/main/java/org/coolapk/gmsinstaller/ui/about/AboutActivity.java
+++ app/src/main/java/org/coolapk/gmsinstaller/ui/about/AboutActivity.java
@@ -5,9 +5,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,27 +31,27 @@ public class AboutActivity extends ActionBarActivity implements View.OnClickList
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("");
 
         TextView versionText = (TextView) findViewById(R.id.about_version);
         versionText.setText(AppHelper.getAppVersionName() + "(" + AppHelper.getAppVersionCode() + ")");
         TextView checkVersionBtn = (TextView) findViewById(R.id.about_check_update);
         checkVersionBtn.setOnClickListener(this);
+        ImageButton likeImage = (ImageButton) findViewById(R.id.about_like_btn);
+        likeImage.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_about, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        if (mLikeCounter == 4) {
+            AVAnalytics.onEvent(this, "shared");
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_like) {
-            onLikeClick();
-            return true;
-        } else if (id == android.R.id.home) {
+        if (item.getItemId() == android.R.id.home) {
             finish();
             return true;
         }
@@ -59,45 +59,12 @@ public class AboutActivity extends ActionBarActivity implements View.OnClickList
         return super.onOptionsItemSelected(item);
     }
 
-    private void onLikeClick() {
-        switch (mLikeCounter) {
-            case 0:
-                Toast.makeText(this, R.string.msg_like1, Toast.LENGTH_SHORT).show();
-                mLikeCounter++;
-                AVAnalytics.onEvent(this, "like1");
-                break;
-            case 1:
-                Toast.makeText(this, R.string.msg_like2, Toast.LENGTH_SHORT).show();
-                mLikeCounter++;
-                AVAnalytics.onEvent(this, "like2");
-                break;
-            case 2:
-                Toast.makeText(this, R.string.msg_like3, Toast.LENGTH_SHORT).show();
-                mLikeCounter++;
-                AVAnalytics.onEvent(this, "like3");
-                break;
-            case 3:
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.btn_share));
-                intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text));
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(Intent.createChooser(intent, getTitle()));
-                mLikeCounter++;
-                AVAnalytics.onEvent(this, "share");
-                break;
-            case 4:
-                Toast.makeText(this, R.string.msg_like4, Toast.LENGTH_SHORT).show();
-                mLikeCounter++;
-                AVAnalytics.onEvent(this, "like4");
-                break;
-        }
-    }
-
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.about_check_update) {
             onUpdateClick();
+        } else if (v.getId() == R.id.about_like_btn) {
+            onLikeClick();
         }
     }
 
@@ -123,5 +90,39 @@ public class AboutActivity extends ActionBarActivity implements View.OnClickList
                 }
             }
         }.execute();
+    }
+
+    private void onLikeClick() {
+        switch (mLikeCounter) {
+            case 0:
+                Toast.makeText(this, R.string.msg_like1, Toast.LENGTH_SHORT).show();
+                AVAnalytics.onEvent(this, "like1");
+                mLikeCounter++;
+                break;
+            case 1:
+                Toast.makeText(this, R.string.msg_like2, Toast.LENGTH_SHORT).show();
+                AVAnalytics.onEvent(this, "like2");
+                mLikeCounter++;
+                break;
+            case 2:
+                Toast.makeText(this, R.string.msg_like3, Toast.LENGTH_SHORT).show();
+                AVAnalytics.onEvent(this, "like3");
+                mLikeCounter++;
+                break;
+            case 3:
+                mLikeCounter++;
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.btn_share));
+                intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(Intent.createChooser(intent, getTitle()));
+                break;
+            case 4:
+                Toast.makeText(this, R.string.msg_like4, Toast.LENGTH_SHORT).show();
+                AVAnalytics.onEvent(this, "like4");
+                mLikeCounter++;
+                break;
+        }
     }
 }

f5a731e5b8f26c2672d63d75019c4fcd4a6febe9
xxxifan <445947962@qq.com>
2015-04-17 14:25:50 +0800

    优化flyme检测逻辑

--- app/src/main/java/org/coolapk/gmsinstaller/util/ViewUtils.java
+++ app/src/main/java/org/coolapk/gmsinstaller/util/ViewUtils.java
@@ -64,13 +64,14 @@ public class ViewUtils {
         boolean result = false;
         if (window != null) {
             try {
-                WindowManager.LayoutParams lp = window.getAttributes();
                 Field darkFlag = WindowManager.LayoutParams.class
                         .getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
                 Field meizuFlags = WindowManager.LayoutParams.class
                         .getDeclaredField("meizuFlags");
                 darkFlag.setAccessible(true);
                 meizuFlags.setAccessible(true);
+
+                WindowManager.LayoutParams lp = window.getAttributes();
                 int bit = darkFlag.getInt(null);
                 int value = meizuFlags.getInt(lp);
                 if (dark) {
